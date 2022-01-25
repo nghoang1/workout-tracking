9201be897a3f150a9aee941d10aa5fec5b0f661b
nghoang1 <57956036+nghoang1@users.noreply.github.com>
2022-01-25 00:34:37 -0600

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-APP_ID = "688babb1"
-API_KEY ="b66b3b3ff4f94f583816ed046548dc07"
-TOKEN = "thisisadrill"
-sheety_endpoint = "https://api.sheety.co/e96e248272a374b1226e293195b65768/myWorkouts/sheet1"
